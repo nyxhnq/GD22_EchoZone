480947f34fe4f6791ec5945c38d3c79f4b0fa913
Nargiza <telagissova.nargiza@gmail.com>
2026-05-24 18:22:30 +0500

    Add Player Animation, changes in PlayerController script🤷‍♀️

--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -5,21 +5,64 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private CharacterController controller;
+    private Animator animator;
 
-    public float speed = 5f;
-    public float turnSpeed = 180f;
+    public float walkSpeed = 5f;
+    public float runSpeed = 10f;
+    public float turnSpeed = 90f;
+
+    private bool isRunning = false;
+
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
-        Vector3 movDir;
+        HandleMovement();
+        HandleAnimation();
+    }
 
-        transform.Rotate(0, Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime, 0);
-        movDir = transform.forward* Input.GetAxis("Vertical") * speed;
+    void HandleMovement()
+    {
+        Vector2 input = InputManager.Instance != null ? InputManager.Instance.GetMoveInput() : Vector2.zero;
+        isRunning = InputManager.Instance != null && InputManager.Instance.IsSprintHeld();
 
-        controller.Move(movDir * Time.deltaTime - Vector3.up * 0.1f);
+        float speed = isRunning ? runSpeed : walkSpeed;
+
+        Vector3 moveDir = new Vector3(input.x, 0, input.y);
+        if (moveDir.magnitude > 1f)
+            moveDir.Normalize();
+
+        if (moveDir != Vector3.zero)
+        {
+            // Поворот в сторону движения
+            Quaternion toRotation = Quaternion.LookRotation(moveDir, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, turnSpeed * Time.deltaTime);
+        }
+
+        controller.Move(moveDir * speed * Time.deltaTime - Vector3.up * 0.1f);
+    }
+
+    void HandleAnimation()
+    {
+        Vector2 input = InputManager.Instance != null ? InputManager.Instance.GetMoveInput() : Vector2.zero;
+        bool isMoving = input.magnitude > 0.1f;
+        isRunning = InputManager.Instance != null && InputManager.Instance.IsSprintHeld();
+
+        if (!isMoving)
+        {
+            animator.Play("Doctor_01|Idle_03");
+        }
+        else if (isMoving && !isRunning)
+        {
+            animator.Play("Doctor_01|Walk_01");
+        }
+        else if (isMoving && isRunning)
+        {
+            animator.Play("Doctor_01|Running_01");
+        }
     }
 }
