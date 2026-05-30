5d6f683a7d75fe4e994ea125bd6d7fc6bd3f4831
Nargiza <telagissova.nargiza@gmail.com>
2026-05-30 18:58:20 +0500

    Add mostly GameScene2 game design

--- Assets/_Scripts/Player/PlayerHealth.cs
+++ Assets/_Scripts/Player/PlayerHealth.cs
@@ -1,49 +1,44 @@
-using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
+//using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using Unity.VisualScripting;
 
-public class PlayerMovement : MonoBehaviour
-{
-   
-}
+//public class PlayerHealth : MonoBehaviour
+//{
+//    [SerializeField] public GameObject hud;
+//    [SerializeField] public GameObject inv;
+//    [SerializeField] public GameObject deathScreen;
+//    [SerializeField] public GameObject player;
 
-public class PlayerHealth : MonoBehaviour
-{
-    [SerializeField] public GameObject hud;
-    [SerializeField] public GameObject inv;
-    [SerializeField] public GameObject deathScreen;
-    [SerializeField] public GameObject player;
+//    [SerializeField] public float health = 100f;
 
-    [SerializeField] public float health = 100f;
+//    // Start is called once before the first execution of Update after the MonoBehaviour is created
+//    void Start()
+//    {
+//        deathScreen.SetActive(false);
+//    }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        deathScreen.SetActive(false);
-    }
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        if (health <= 0)
+//        {
+//            // Ensure PlayerMovement is a valid component
+//            var playerMovement = player.GetComponent<PlayerMovement>();
+//            if (playerMovement != null)
+//            {
+//                playerMovement.enabled = false;
+//            }
+//            Cursor.visible = true;
+//            Cursor.lockState = CursorLockMode.None;
+//            hud.SetActive(false);
+//            inv.SetActive(false);
+//            deathScreen.SetActive(true);
+//        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (health <= 0)
-        {
-            // Ensure PlayerMovement is a valid component
-            var playerMovement = player.GetComponent<PlayerMovement>();
-            if (playerMovement != null)
-            {
-                playerMovement.enabled = false;
-            }
-            Cursor.visible = true;
-            Cursor.lockState = CursorLockMode.None;
-            hud.SetActive(false);
-            inv.SetActive(false);
-            deathScreen.SetActive(true);
-        }
-
-        if (health > 100)
-        {
-            health = 100f;
-        }
-    }
-}
+//        if (health > 100)
+//        {
+//            health = 100f;
+//        }
+//    }
+//}
--- Assets/_Scripts/UI/HealthBar.cs
+++ Assets/_Scripts/UI/HealthBar.cs
@@ -1,24 +1,24 @@
-using UnityEngine;
-using UnityEngine.UI;
-using System.Collections;
-using System.Collections.Generic;
-public class HealthBar : MonoBehaviour
-{
-    [SerializeField] private Image healthBar;
-    [SerializeField] public float CurrentHealth;
-    [SerializeField] private float MaxHealth = 100f;
-    PlayerHealth player;
+//using UnityEngine;
+//using UnityEngine.UI;
+//using System.Collections;
+//using System.Collections.Generic;
+//public class HealthBar : MonoBehaviour
+//{
+//    [SerializeField] private Image healthBar;
+//    [SerializeField] public float CurrentHealth;
+//    [SerializeField] private float MaxHealth = 100f;
+//    PlayerHealth player;
 
-    void Start()
-    {
-        healthBar = GetComponent<Image>();
-        player = FindObjectOfType<PlayerHealth>();
-    }
+//    void Start()
+//    {
+//        healthBar = GetComponent<Image>();
+//        player = FindObjectOfType<PlayerHealth>();
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        CurrentHealth = player.health;
-        healthBar.fillAmount = CurrentHealth / MaxHealth; 
-    }
-}
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        CurrentHealth = player.health;
+//        healthBar.fillAmount = CurrentHealth / MaxHealth; 
+//    }
+//}
